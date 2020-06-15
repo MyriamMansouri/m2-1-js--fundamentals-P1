91c66ceb51d93abb269ad01659ccc39c0cfbe8af
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-15 15:28:32 -0400

    Exercise 9

--- workshop/exercise-9.js
+++ workshop/exercise-9.js
@@ -24,3 +24,18 @@
 // FizzBuzz
 // 16
 // ...
+
+for (let num = 1 ; num < 101 ; num ++ ) {
+    let print = '';
+    if (num % 3 === 0 && num % 5 === 0 ) {
+        print = 'FizzBuzz';
+    } else if (num % 3 === 0) {
+        print = 'Fizz'
+    } else if (num % 5 === 0) {
+        print = 'Buzz'
+    } else {
+        print = num
+    }
+    console.log(print);
+    
+}
