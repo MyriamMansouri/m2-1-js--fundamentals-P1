281183dd0ce1270c6fbc471b922d5e812275527a
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-15 15:13:44 -0400

    Exercise 8

--- workshop/exercise-8.js
+++ workshop/exercise-8.js
@@ -3,3 +3,10 @@
 // between 0 and 5000
 
 // ANSWER: the number you should see in the console is 3127500
+let sum = 0;
+for (let num = 0 ; num < 5001 ; num ++ ) {
+    if (num % 4 === 0) {
+        sum = num + sum ;
+    }
+}
+console.log(sum)
